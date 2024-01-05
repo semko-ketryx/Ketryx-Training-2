96c0e38d5fca6a9c515927c9fe775a82c2aa7a03
Gabriel Pascualy <gabrielp@ketryx.com>
2024-01-05 08:44:30 -0500

    Update page.test.tsx

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -77,14 +77,3 @@ describe('Unit test bluetooth connectivity @tests:KT-3', () => {
   })
 })
 
-describe('Unit test Android Client (Patient App) @tests:KXITM7VNAYPPX2686NT493MKDMZRNWZ', () => {
-  it('Verify App Opens/Closes', () => {
-    render(<Home />)
-
-    const heading = screen.getByRole('heading', {
-      name: /Docs ->/i,
-    })
-
-    expect(heading).toBeInTheDocument()
-  })
-})
